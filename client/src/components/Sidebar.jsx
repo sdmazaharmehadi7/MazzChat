be99f6c9c64636ebdf7211de71b95440fb46eaea
mazahar <mazaharmazahar504@gmail.com>
2026-06-04 23:05:22 +0530

    updated the profile edit option

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
             <div className="relative py-2 group">  
                 <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
         <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
-                    <p onclick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+                    <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
                     <hr className="my-2 border-t border-gray-500"/>
                     <p className='cursor-pointer text-sm'>Logout</p>
                 </div>
