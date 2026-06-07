e413c625ef5a4fd819c8fe91ce07acd617e45b5c
mazahar <mazaharmazahar504@gmail.com>
2026-06-07 19:25:59 +0530

    backend backend created and connected to frontend lets cook now

---
+++ server/controllers/userController.js
@@ -0,0 +1,87 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js"
+
+// signup a new user
+
+export const signup = async (req,res)=>{
+    const{ fullName ,email ,password,bio}=req.body;
+
+    try{
+        if( !fullName || !email || !password || !bio){
+            return res.json({success:false,message:"Missing Details"})
+        }
+        const user =await User.findOne({email})
+        if(user){
+            return res.json({success:false,message:"Account already exists"})
+        }
+
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt)
+
+        const newUser =await User.create({
+            fullName,email,password: hashedPassword,bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success: true,userData:newUser,token,message:"Account created successfully"})
+
+    } catch (error) {
+        console.log(error.message);
+         res.json({success: false,message:error.message})
+
+    }
+}
+
+//controller to login a user
+export const login=async(req,res)=>{
+    try {
+        const {email,password}=res.body; 
+        const userData=await User.findOne({email})
+
+        const isPasswordCorrect =await bcrypt.compare(password,userData.password);
+
+        if(!isPasswordCorrect){
+            return res.json({success:false,message: "Invalid credentials"});
+        }
+
+         const token = generateToken(userData._id)
+
+        res.json({success: true,userData,token,message:"Login Successful"})
+    }catch(error){
+        console.log(error.message);
+         res.json({success: false,message:error.message})
+    }
+
+}
+
+//controller to check if the user is authenticated
+export const checkAuth = (req,res)=>{
+    res.json({success: true,user:req.user});
+}
+
+//controller to update user profile
+export const updateProfile=async (req,res)=>{
+    try{
+        const {profilePic,bio,fullName}=req.body;
+
+        const userId=req.user._id;
+        let updateddUser;
+
+        if(!profilePic){
+            await User.findByIdAndUpdate(userId,{bio,fullName},
+            {new:true});
+        }else{
+            const upload=await cloudinary.uploader.upload(profilePic);
+
+            updatedUser =await User.findByIdAndUpdate(userId,{profilePic:upload,secure_url,bio,fullName},{new:true});
+        }
+
+        res.json({success:true,user:updateddUser})
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,17 +4,48 @@ import cors from "cors";
 import http from "http";
 import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 //create Express app and Http server
 
 const app =express();
 const server =http.createServer(app)
 
+//initialize socket.io server
+export const io=new Server(server,{
+    cors:{origin:"*"}
+})
+
+//store online users
+export const userSocketMap={}; //{userId:socketId}
+
+//socket.io connection handeler
+io.on("connection",(socket)=>{
+    const userId =socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId]=socket.id;
+
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
+
 //middleware setup
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+
+//routes setup
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter)
 
 //connect to mongodb
 await connectDB();
