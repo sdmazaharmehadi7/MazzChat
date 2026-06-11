6b63e0b2380b389f66dca14d0dfee84bf09eb4d9
mazahar <mazaharmazahar504@gmail.com>
2026-06-12 00:40:35 +0530

    final one

--- server/server.js
+++ server/server.js
@@ -58,3 +58,5 @@ if(process.env.NODE_ENV !== "production"){
     const PORT=process.env.PORT || 8000;
 server.listen(PORT,()=>console.log("Server is running on PORT :"+PORT));
 }
+
+export default server;
